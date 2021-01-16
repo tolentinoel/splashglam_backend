7cccc8d7e0a56614e0ebe398035348a48e6abbc4
tolentinoel <rckrleyn@gmail.com>
2021-01-16 02:33:22 -0800

    starting JWT

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :lists, dependent: :destroy
     has_many :products, through: :lists
-    # has_secure_password
+    has_secure_password
     validates :username, uniqueness: true
     validates :name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -65,24 +65,6 @@ end
 ellaine = User.create({name: "ellaine", username: "ellainet", skin_type: "oily", age: 28})
 kim = User.create({name: "kim", username: "nnhk23", skin_type: "combination", age: 21})
 
-# product1 = Product.create({
-#     name: "Moisture Boost Gel To Water Morning Cleanser",
-#     url: "https://sokoglam.com/collections/oily/products/tonymoly-moisture-boost-gel-to-water-morning-cleanser",
-#     brand: "TONY MOLY",
-#     brand_url: "https://sokoglam.com/collections/tony-moly",
-#     price: "$13.50",
-#     image_url: "https://cdn.shopify.com/s/files/1/0249/1218/products/1.8SokoGlamPDP-TonyMolyMoistureBoostGelToWaterCleanser_180ml__Package_860x.jpg?v=1610510465",
-#     description: "Made with gentle, naturally-derived surfactants for use on even the most sensitive skin, this clinically tested, low pH cleanser transforms from a light gel texture to water as you massage it in. After using, skin will feel hydrated, soothed and properly cleansed. Contains 5 types of centella asiatica extracts, a panthenol and ceramide complex, as well as AHA, BHA and PHAs. Hydrates skin while gently removing dirt and dead skin cells for a soothing cleanse - a perfect way to begin the morning!"})
-
-# product2 = Product.create({
-#     name: "Deep Green Tea Serum",
-#     url: "https://sokoglam.com/collections/oily/products/benton-deep-green-tea-serum",
-#     brand: "BENTON",
-#     brand_url: "https://sokoglam.com/collections/benton",
-#     price: "$17",
-#     image_url: "https://cdn.shopify.com/s/files/1/0249/1218/products/12.7SokoGlamPDP-BentonDeepGreenTeaSerum_860x.jpg?v=1607989680",
-#     description: "Rebalance and revive dull, rough skin with this nourishing serum. Containing over 53% green tea extracts from BoSeong, the home of green tea farming in Korea, to gently address chapped, dehydrated and irritated skin with a calming cooling effect."
-# })
 
 List.create({title: "firstList", product: Product.first, user: ellaine})
 Review.create({content: "My first ever review to a product here in SplashGlam! This is to test if my relationships are working! If you read this. Yay!", user: kim, product: Product.first})
